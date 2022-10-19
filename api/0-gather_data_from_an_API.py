7ccf5a8b14f641d020450baa6f4969947a127519
misskukuua <p.asare@alustudent.com>
2022-10-19 13:51:29 -0700

    task 1 updated

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,8 +6,10 @@ from sys import argv
 
 if __name__ == '__main__':
     employee_id = argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
+        .format(employee_id)
     user_response = requests.get(user_url).json()
     todos_response = requests.get(todos_url).json()
 
@@ -19,16 +21,10 @@ if __name__ == '__main__':
             total_tasks += 1
         if item.get('completed'):
             completed_tasks += 1
-    print("Employee {} is done with tasks ({}/{}):".format(user_response.get('name'),
-                                                           completed_tasks, total_tasks))
+
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(user_response.get('name'),
+                  completed_tasks, total_tasks))
     for todo in todos_response:
         if todo.get('completed'):
             print("\t {}".format(todo.get('title')))
-
-    # res = requests.get(todos_url)
-    # try:
-    #     todos_response = res.json()
-    # except requests.exceptions.JSONDecodeError:
-    #     print("exfjkhlk")
-    #
-    # print(todos_response)
