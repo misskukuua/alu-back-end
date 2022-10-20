617ce33c5610e5719e86b1767bcf1f5c8f9fbcd9
misskukuua <p.asare@alustudent.com>
2022-10-20 12:31:31 -0700

    task 3 updated

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,7 +4,9 @@
 import requests
 from sys import argv
 
+import task
 from pygments.lexers import q
+
 
 if __name__ == '__main__':
     employee_id = argv[1]
@@ -29,8 +31,8 @@ if __name__ == '__main__':
         todo_title = todo.get('title')
         todo_status = todo.get('completed')
         with open(str(user_id) + '.csv', 'w') as file:
-            [file.write('"' + str(user_id) + '",' +
+            [file.write('"' + user_id + '",' +
                         '"' + user_name + '",' +
                         '"' + str(todo_status) + '",' +
-                        '"' + todo_title + '"' + "\n")
-             for todo in todos_response.format(q)]
+                        '"' + todo_title + '",' + "\n")
+             for todo in todos_response]
