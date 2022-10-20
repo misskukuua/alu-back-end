2ba59dda396b268db0bae1b238bbea04bc973bb9
misskukuua <p.asare@alustudent.com>
2022-10-20 02:24:10 -0700

    task 3 updated

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -11,12 +11,10 @@ if __name__ == '__main__':
         .format(employee_id)
     todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos" \
         .format(employee_id)
-    
+
     user_response = requests.get(user_url).json()
     todos_response = requests.get(todos_url).json()
 
-
-    #user_id = str(employee_id)
     user_name = user_response.get('username')
 
     todos_information = [
@@ -25,5 +23,5 @@ if __name__ == '__main__':
         for task in todos_response]
 
     user_dict = {str(employee_id): todos_information}
-    with open(str(user_id) + '.json', "w") as file:
+    with open(str(employee_id) + '.json', "w") as file:
         file.write(json.dumps(user_dict))
