ecc0b490449bf1b68d13303fc81e20b4302b3b4f
misskukua <p.asare@alustudent.com>
2022-10-21 00:15:45 +0000

    :robot: automated commit of one or many files

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -13,22 +13,4 @@ if __name__ == '__main__':
     user_response = requests.get(user_url).json()
     todos_response = requests.get(todos_url).json()
 
-    total_tasks = 0
-    completed_tasks = 0
-
-    for item in todos_response:
-        if item.get('completed'):
-            completed_tasks += 1
-
-    user_id = str(employee_id)
-    user_name = user_response.get('username')
-
-    for todo in todos_response:
-        todo_title = todo.get('title')
-        todo_status = todo.get('completed')
-        with open(str(user_id) + '.csv', 'w') as file:
-            [file.write('"' + user_id + '",' +
-                        '"' + user_name + '",' +
-                        '"' + str(todo_status) + '",' +
-                        '"' + todo_title + '",' + "\n")
-             for todo in todos_response]
+    total_             for todo in todos_response]
